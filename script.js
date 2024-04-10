0a68f52ece8318059f41a4f81df69e51e34ef470
praveen-rikhari <praveenrikhari45@gmail.com>
2024-04-10 17:20:56 +0530

    creating cubic curves

--- script.js
+++ script.js
@@ -55,12 +55,24 @@ ctx.fillStyle = "#23b0ed"
 // ctx.stroke();
 
  // Quadratic curves example
- ctx.beginPath();
- ctx.moveTo(75, 25);
- ctx.quadraticCurveTo(25, 25, 25, 62.5);
- ctx.quadraticCurveTo(25, 100, 50, 100);
- ctx.quadraticCurveTo(50, 120, 30, 125);
- ctx.quadraticCurveTo(60, 120, 65, 100);
- ctx.quadraticCurveTo(125, 100, 125, 62.5);
- ctx.quadraticCurveTo(125, 25, 75, 25);
- ctx.stroke();
+//  ctx.beginPath();
+//  ctx.moveTo(75, 25);
+//  ctx.quadraticCurveTo(25, 25, 25, 62.5);
+//  ctx.quadraticCurveTo(25, 100, 50, 100);
+//  ctx.quadraticCurveTo(50, 120, 30, 125);
+//  ctx.quadraticCurveTo(60, 120, 65, 100);
+//  ctx.quadraticCurveTo(125, 100, 125, 62.5);
+//  ctx.quadraticCurveTo(125, 25, 75, 25);
+//  ctx.stroke();
+
+
+// Cubic curves example
+ctx.beginPath();
+ctx.moveTo(75, 40);
+ctx.bezierCurveTo(75, 37, 70, 25, 50, 25);
+ctx.bezierCurveTo(20, 25, 20, 62.5, 20, 62.5);
+ctx.bezierCurveTo(20, 80, 40, 102, 75, 120);
+ctx.bezierCurveTo(110, 102, 130, 80, 130, 62.5);
+ctx.bezierCurveTo(130, 62.5, 130, 25, 100, 25);
+ctx.bezierCurveTo(85, 25, 75, 37, 75, 40);
+ctx.fill();

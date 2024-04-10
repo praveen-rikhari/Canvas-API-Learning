afd4459b315a3f173445aff26698c7ceb3ca0b07
praveen-rikhari <praveenrikhari45@gmail.com>
2024-04-10 16:04:33 +0530

    creating cicrcle and smile emoji

--- script.js
+++ script.js
@@ -16,8 +16,23 @@ ctx.fillStyle = "#23b0ed"
 
 
 // creating triangle using paths
+// ctx.beginPath();
+// ctx.moveTo(75, 50);
+// ctx.lineTo(100, 75);
+// ctx.lineTo(100, 25);
+// ctx.fill();
+
+// creating circle and smile emoji using arc
+
 ctx.beginPath();
-ctx.moveTo(75, 50);
-ctx.lineTo(100, 75);
-ctx.lineTo(100, 25);
-ctx.fill();
+ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+ctx.moveTo(110, 75);
+
+ctx.arc(75, 75, 35, 0, Math.PI, false); // Mouth (clockwise)
+ctx.moveTo(65, 65);
+
+ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Left eye
+ctx.moveTo(95, 65);
+
+ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Right eye
+ctx.stroke();

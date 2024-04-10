50c06f5d545c0a3020196d2f672b8c7cbf369ed8
praveen-rikhari <praveenrikhari45@gmail.com>
2024-04-10 16:07:38 +0530

    creating shapes lines

--- script.js
+++ script.js
@@ -24,15 +24,32 @@ ctx.fillStyle = "#23b0ed"
 
 // creating circle and smile emoji using arc
 
+// ctx.beginPath();
+// ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
+// ctx.moveTo(110, 75);
+
+// ctx.arc(75, 75, 35, 0, Math.PI, false); // Mouth (clockwise)
+// ctx.moveTo(65, 65);
+
+// ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Left eye
+// ctx.moveTo(95, 65);
+
+// ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Right eye
+// ctx.stroke();
+
+//creating traiangle using lines
+
+// Filled triangle
 ctx.beginPath();
-ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Outer circle
-ctx.moveTo(110, 75);
+ctx.moveTo(25, 25);
+ctx.lineTo(105, 25);
+ctx.lineTo(25, 105);
+ctx.fill();
 
-ctx.arc(75, 75, 35, 0, Math.PI, false); // Mouth (clockwise)
-ctx.moveTo(65, 65);
-
-ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Left eye
-ctx.moveTo(95, 65);
-
-ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Right eye
+// Stroked triangle
+ctx.beginPath();
+ctx.moveTo(125, 125);
+ctx.lineTo(125, 45);
+ctx.lineTo(45, 125);
+ctx.closePath();
 ctx.stroke();

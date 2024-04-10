1e7b097166c23ac6974965bb5068f57500509f30
praveen-rikhari <praveenrikhari45@gmail.com>
2024-04-10 15:44:51 +0530

    Creating shapes

---
+++ script.js
@@ -0,0 +1,15 @@
+var canvas = document.getElementById("myCanvas");
+console.log("Hello");
+// using getContext object for creating shapes and drawing
+var ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "#23b0ed"
+
+// Draws a filled rectangle.
+ctx.fillRect(25, 25, 100, 100);
+
+// Clears the specified rectangular area, making it fully transparent.
+ctx.clearRect(45, 45, 60, 60);
+
+// Draws a rectangular outline.
+ctx.strokeRect(50, 50, 50, 50);

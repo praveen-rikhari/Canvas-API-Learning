b0b974058bfb894eb25fb6a8cc68c41ec6b992bc
praveen-rikhari <praveenrikhari45@gmail.com>
2024-04-10 18:02:10 +0530

    creating radial gradient

---  Color & Gradient/script.js
+++  Color & Gradient/script.js
@@ -1,19 +1,31 @@
-var canvas = document.getElementById("myCanvas");
-console.log("Hello");
+var lgCanvas = document.getElementById("lgCanvas");
 // using getContext object for creating shapes and drawing
-var ctx = canvas.getContext("2d");
+var ctx = lgCanvas.getContext("2d");
 
 // creating linear gradient
-var grd = ctx.createLinearGradient(0, 0, 200, 0)
-grd.addColorStop(0, "lightblue");
-grd.addColorStop(1, "darkblue");
+var lGrad = ctx.createLinearGradient(0, 0, 200, 0)
+lGrad.addColorStop(0, "lightblue");
+lGrad.addColorStop(1, "darkblue");
 
 // fill rectangle with gradient
-ctx.fillStyle = grd;
+ctx.fillStyle = lGrad;
 ctx.fillRect(10,10,150,80);
 
 // Fill outlined rectangle with gradient
 ctx.lineWidth = 10;
-ctx.strokeStyle = grd;
+ctx.strokeStyle = lGrad;
 ctx.strokeRect(10,100,150,80)
 
+
+// Radial Gradient
+var rgCanvas = document.getElementById("rgCanvas");
+var ctx1 = rgCanvas.getContext("2d");
+
+// Create gradient
+const rGrad=ctx1.createRadialGradient(150,75,15,150,75,150);
+rGrad.addColorStop(0,"lightblue");
+rGrad.addColorStop(1,"darkblue");
+
+// Fill rectangle with gradient
+ctx1.fillStyle = rGrad;
+ctx1.fillRect(10,10,280,130);

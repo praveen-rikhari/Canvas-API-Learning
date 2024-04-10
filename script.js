711178b250fb7af16bf6d7a8606aeeda2193ee04
praveen-rikhari <praveenrikhari45@gmail.com>
2024-04-10 15:49:57 +0530

    creating paths

--- script.js
+++ script.js
@@ -6,10 +6,18 @@ var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#23b0ed"
 
 // Draws a filled rectangle.
-ctx.fillRect(25, 25, 100, 100);
+// ctx.fillRect(25, 25, 100, 100);
 
 // Clears the specified rectangular area, making it fully transparent.
-ctx.clearRect(45, 45, 60, 60);
+// ctx.clearRect(45, 45, 60, 60);
 
 // Draws a rectangular outline.
-ctx.strokeRect(50, 50, 50, 50);
+// ctx.strokeRect(50, 50, 50, 50);
+
+
+// creating triangle using paths
+ctx.beginPath();
+ctx.moveTo(75, 50);
+ctx.lineTo(100, 75);
+ctx.lineTo(100, 25);
+ctx.fill();

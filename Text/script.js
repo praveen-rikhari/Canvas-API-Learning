c315ced728c9f4c2231d8bec8519bf95aefeae58
praveen-rikhari <praveenrikhari45@gmail.com>
2024-04-10 18:09:11 +0530

    created filled text and color

--- Text/script.js
+++ Text/script.js
@@ -2,7 +2,12 @@ const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 ctx.font = "50px Arial";
+ctx.strokeStyle = "red";
 ctx.strokeText("Hello World",10,80);
 
 ctx.font = "bold italic 50px Arial";
 ctx.strokeText("Hello World",10,160);
+
+ctx.font = "bold italic 50px Arial";
+ctx.fillStyle = "orange"
+ctx.fillText("Hello World",10,240);
